c13937dbc6d8609949b9611c0112af5fc1738d60
Viktar Mushtsin <vmushtsin@ibagroup.eu>
2019-05-15 18:31:42 +0300

    kek

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ const Telegraf = require('telegraf')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 //bot.command('joke', ({ reply }) => reply('МАКСИМ ПАНК'))
-bot.hears(new RegExp('[\\?&]' + 'Максим' + '=([^&#]*)', 'i'), ({reply}) => reply('ПАНК'))
+bot.hears(new RegExp('[\\?&]' + 'Максим' + '=([^&#]*)', 'i'), ({ reply }) => reply('ПАНК'))
+bot.command('joke', ({
+  reply,
+  sender = from.sender
+}) => reply(`${sender === 'Neksus_5' ? 'ТЫ' : 'МАКСИМ'} ПАНК`))
+bot.start(({ reply }) => reply('Hi'))
 bot.launch()
