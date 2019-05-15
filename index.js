7b40abb62c5e53575af67e63d3e7d72670df4430
Viktar Mushtsin <vmushtsin@ibagroup.eu>
2019-05-15 18:25:01 +0300

    punks

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const Telegraf = require('telegraf')
 
-/* bot.context.db = {
-  getJokeBySender: sender => `${(sender === 'Neksus_5' ? 'ТЫ' : 'МАКСИМ')} ПАНК`
-} */
-
 const bot = new Telegraf(process.env.BOT_TOKEN)
 //bot.command('joke', ({ reply }) => reply('МАКСИМ ПАНК'))
 bot.hears(new RegExp('[\\?&]' + 'Максим' + '=([^&#]*)', 'i'), ({reply}) => reply('ПАНК'))
