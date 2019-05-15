fe75d44a4dc4d316e269dfbc2e54b15148ce042a
Viktar Mushtsin <vmushtsin@ibagroup.eu>
2019-05-15 18:54:07 +0300

    punk3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Telegraf = require('telegraf')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 //bot.command('joke', ({ reply }) => reply('МАКСИМ ПАНК'))
-bot.hears(new RegExp('[\\?&]' + 'Максим' + '=([^&#]*)', 'i'), ({ reply }) => reply('ПАНК'))
+bot.hears('Максим', ({ reply }) => reply('ПАНК'))
 bot.command('joke', ({
   reply,
   message
