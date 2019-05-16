4ef20ca40911c4734ca4f2b79b53bc5c0c515e87
Viktar Mushtsin <vmushtsin@ibagroup.eu>
2019-05-16 15:06:21 +0300

    refactoring 1

--- index.js
+++ index.js
@@ -1,52 +1,5 @@
 const Telegraf = require('telegraf')
-
-const russianVowels = [
-  'а',
-  'и',
-  'о',
-  'э',
-  'у',
-  'я',
-  'ы',
-  'ё',
-  'е',
-  'ю',
-]
-
-const convertVowel = vowel => {
-  switch (vowel) {
-    case 'а':
-      return 'Хуя'
-    case 'о':
-      return 'Хуё'
-    case 'э':
-      return 'Хуэ'
-    case 'у':
-      return 'Хую'
-    default:
-      return `Ху${vowel}`
-  }
-}
-
-const convertWord = word => {
-  const vowelNum = word
-    .toLowerCase()
-    .split('')
-    .findIndex(elem => russianVowels.includes(elem))
-  if (vowelNum !== -1) {
-    const convertedVowel = convertVowel(word.toLowerCase().charAt(vowelNum))
-    const removedFirst = word.slice(vowelNum + 1)
-    return convertedVowel + removedFirst
-  } else {
-    return `Ху${word.toLowerCase()}`
-  }
-}
-
-const makeHui = string => {
-  const sentence = string.split(' ')
-  const lastWord = sentence[sentence.length - 1]
-  return convertWord(lastWord)
-}
+const { makeHui } = require('./make-hui.js')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
