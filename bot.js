1cd1ec88dc6331030b881c40366b0492b3bc97aa
Viktar Mushtsin <vmushtsin@ibagroup.eu>
2019-05-20 19:36:15 +0300

    express added

---
+++ bot.js
@@ -0,0 +1,42 @@
+const token = process.env.TOKEN
+
+const Bot = require('telegraf')
+const { makeHui } = require('./make-hui')
+let bot
+
+if(process.env.NODE_ENV === 'production') {
+  bot = new Bot(token)
+  bot.setWebHook(process.env.HEROKU_URL + bot.token)
+}
+else {
+  bot = new Bot(token, { polling: true })
+}
+
+console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode')
+
+let lastWordByChat = {}
+
+bot.context.db = {
+  getLastMessageTextByChatId: id => lastWordByChat[id],
+  setLastMessageTextByChatId: (id, text) => lastWordByChat[id] = text
+}
+
+bot.start(({ reply }) => reply('Hi'))
+
+/* bot.on('inline_query', ctx => {
+  const query = ctx.update.inline_query.query
+}) */
+
+bot.command('hui', async ({ reply, db, message }) => {
+  reply(makeHui(db.getLastMessageTextByChatId(message.chat.id)))
+})
+bot.hears(/максим/i, ({ reply }) => reply('ПАНК'))
+
+
+bot.on('text', async ({ message, db }) => {
+  db.setLastMessageTextByChatId(message.chat.id, message.text)
+})
+
+bot.launch()
+
+module.exports = bot;
--- index.js
+++ index.js
@@ -1,33 +1,4 @@
-const Telegraf = require('telegraf')
-const { makeHui } = require('./make-hui.js')
+require("dotenv").config();
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-
-let lastWordByChat = {}
-
-bot.context.db = {
-  getLastMessageTextByChatId: id => lastWordByChat[id],
-  setLastMessageTextByChatId: (id, text) => lastWordByChat[id] = text
-}
-
-bot.start(({ reply }) => reply('Hi'))
-
-bot.on('inline_query', ctx => {
-  const query = ctx.update.inline_query.query
-  
-})
-
-bot.command('hui', async ({ reply, db, message }) => {
-  reply(makeHui(db.getLastMessageTextByChatId(message.chat.id)))
-})
-bot.hears(/максим/i, ({ reply }) => reply('ПАНК'))
-
-
-bot.on('text', async ({ message, db }) => {
-  db.setLastMessageTextByChatId(message.chat.id, message.text)
-})
-
-bot.launch()
-
-
-//console.log(makeHui('Привет сосать'))
+var bot = require('./bot');
+require('./web')(bot);
