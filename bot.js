7ed9b5d4ca190141fd3a7a208f75ca3c8d8a0635
Viktar Mushtsin <vmushtsin@ibagroup.eu>
2019-05-20 19:40:58 +0300

    webhook fixed

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ let bot
 
 if(process.env.NODE_ENV === 'production') {
   bot = new Bot(token)
-  bot.setWebHook(process.env.HEROKU_URL + bot.token)
+  bot.telegram.setWebHook(process.env.HEROKU_URL + bot.token)
 }
 else {
   bot = new Bot(token, { polling: true })
