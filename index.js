c4be31b1cf48a093a5d66b481d2ba3b5421deeea
Viktar Mushtsin <vmushtsin@ibagroup.eu>
2019-05-21 14:45:45 +0300

    Prevent from sleeping

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-var { bot } = require('./bot');
+const { bot } = require('./bot');
 require('./web')(bot);
+const http = require("http");
+setInterval(
+  () => http.get(process.env.HEROKU_URL),
+  300000
+)
